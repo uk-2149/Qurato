a2efb96f91739b117501b7bed1683e1f7720629c
uk-2149 <utkal2149@gmail.com>
2026-01-09 17:49:31 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppProviders from "@/components/providers/AppProviders";
 import TitleWatcher from "../components/TitleWatcher";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <AppProviders>
           <TitleWatcher />
           {children}
+          <Analytics />
         </AppProviders>
       </body>
     </html>
